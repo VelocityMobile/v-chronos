884e24e7e6eca2403f7148e69e2ce88ad17f4727
evanjmg@gmail.com <evanjmg@gmail.com>
2017-10-11 17:30:24 +0100

    Fix comment

--- src/models/time.ts
+++ src/models/time.ts
@@ -1,5 +1,5 @@
 
 export interface VTimeEvent {
-  start: string // e.g 15:22:42+1:00
-  end: string // e.g 19:22:42+1:00
+  start: string // e.g 15:22:42+01:00
+  end: string // e.g 19:22:42+01:00
 }
